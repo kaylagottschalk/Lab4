681560aa4fbd534c2ed4df0e302da9c22652e988
kaylagottschalk <33965231+kaylagottschalk@users.noreply.github.com>
2017-12-05 23:46:55 -0500

    Add files via upload

---
+++ week7/week7/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoop1_Click(object sender, EventArgs e)
+            //for
+        {   // remove the intructions from the label
+            lblMessage1.Text = "";
+
+            // use a loop to display #'s from 100 to 120 on our label
+            // for lopp structure: Start #, condition for continuing, increment
+        
+            for (Int32 counter = 100; counter <= 120; counter++)
+
+            { 
+                //add the current # on to the end of the label text
+                lblMessage1.Text += counter + " "; 
+
+
+            } 
+        }
+        //while
+        private void btnWhileLoop_Click(object sender, EventArgs e)
+        {
+            // remove the intructions from the label
+            lblMessage1.Text = "";
+
+            // use a loop to display #'s from 100 to 120 on our label
+            // for lopp structure: Start #, condition for continuing, increment
+            Int32 counter = 100;
+
+            while (counter <= 120)
+            {
+                //add the current # on to the end of the label text
+                lblMessage1.Text += counter + " ";
+
+                //add 1 to the counter 
+                counter++;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //clear the message label
+            lblSum.Text = "";
+
+            // capture x and y and try to convert them to numeric variables
+            //Int32 x = Convert.ToInt32(txtX.Text);
+            //Int32 y = Convert.ToInt32(txtY.Text);
+
+            //assume the form is complete
+            bool ok = true;
+
+            //check if the x input is numeric
+
+            if (Int32.TryParse(txtX.Text, out Int32 x) == false)
+            {
+                lblSum.Text = "Enter a number for x.";
+                ok = false;
+            }
+            else if (x <0)
+            {
+                lblSum.Text = "Enter a positive number for x.";
+            }
+
+            //check is the y input is numeric
+
+            if (Int32.TryParse(txtY.Text, out Int32 y) == false)
+            {
+                lblSum.Text = "Enter a number for y.";
+                ok = false;
+            }
+            else if (y <0)
+            {
+                lblSum.Text = "Enter a positive number for y.";
+            }
+
+            // send x and y to the AddNumbers method & display the result
+            if (ok == true)
+            {
+                Int32 result = AddNumbers(x, y);
+                lblSum.Text = Convert.ToString(result);
+            }
+
+        }
+        // method accepts 2 integer inputs and returns another integer back
+        private Int32 AddNumbers(Int32 x, Int32 y)
+        {
+            // add the 2 input parameter values together 
+            Int32 sum = x + y;
+
+            // send back result
+            return sum; 
+
+
+        }
+    }
+}
